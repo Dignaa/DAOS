1a3611a774860e0ef57a9c15888212d1be244543
Dignaa <dbringule@gmail.com>
2024-11-04 14:48:26 +0100

    Bug fix

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,6 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: (errors) => {
