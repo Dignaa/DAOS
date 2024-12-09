c381e3d28b77d307342fd6a3fa1d5b4750e7e356
Dignaa <dbringule@gmail.com>
2024-12-09 15:39:51 +0100

    Deploy api

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,32 +4,43 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './exception-filters';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
 const server = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.enableCors();
-  app.useGlobalFilters(new HttpExceptionFilter());
 
+  app.enableCors();
+
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: (errors) => {
-        // Transform errors to include field and message
         const formattedErrors = errors.map((error) => ({
           field: error.property,
           message: Object.values(error.constraints).join(', '),
         }));
-
-        // Throw BadRequestException with formatted errors
         return new BadRequestException(formattedErrors);
       },
     }),
   );
 
-  const port = 3000;
-  await app.listen(port);
-  console.log(`Nest application is running on: http://localhost:${port}`);
-}
-bootstrap();
+  await app.init();
 
-export const handler = server;
+  return app;
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap().then((app) => {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`NestJS app is running locally at http://localhost:${port}`);
+    });
+  });
+}
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  const app = await bootstrap();
+  app.getHttpAdapter().getInstance()(req, res);
+};
